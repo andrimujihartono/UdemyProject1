eae3248d202e11c35e77759fda30e8ed9654b102
andrimuji69 <andrimuji69@gmail.com>
2022-06-08 21:50:39 +0700

    section 16 Reading the data from the databse in the api

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,19 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
         {
-            return "this will list products";
+            _context = context;
         }
-        [HttpGet("{id}")]
-        public string GetProduct(int id)
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "single Products";
+            var products = await  _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+            
         }
     }
 }
