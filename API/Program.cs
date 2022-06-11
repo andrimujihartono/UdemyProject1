c25f90a3d7b333382edf5f54bed62ae90ed84323
andrimuji69 <andrimuji69@gmail.com>
2022-06-12 00:17:54 +0700

    Adding seed data

--- API/Program.cs
+++ API/Program.cs
@@ -22,6 +22,8 @@ namespace API
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                    
                 }catch(Exception ex)
                 {
                     var logger = loggerFactory.CreateLogger<Program>();
