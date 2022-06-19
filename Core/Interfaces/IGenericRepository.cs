8102ffa6e16ee8b250af4e10366894feb4a48db1
andrimuji69 <andrimuji69@gmail.com>
2022-06-19 16:41:11 +0700

    initial commit

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Spesifications;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpesification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpesification<T> spec);
+         
+    }
+}
